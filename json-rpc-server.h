8d17f56ee01b0ef7c4a0e54661917d962cd28e01
Henner Zeller <h.zeller@acm.org>
2021-09-30 17:36:16 -0700

    Dispatch to Request or Notification depending on existence of "id".

--- json-rpc-server.h
+++ json-rpc-server.h
@@ -18,34 +18,29 @@
 
 class JsonRpcServer {
 public:
-  using RPCHandler = std::function<ResponseMessage (const RequestMessage& r)>;
+  using RPCHandler = std::function<nlohmann::json (const nlohmann::json& r)>;
+  using RPCNotification = std::function<void (const nlohmann::json& r)>;
 
   // Some statistical counters.
   using StatsMap = std::map<std::string, int>;
 
-  // Write a list of string_views to the output. Each call to this write
-  // function represents a complete response.
-  // Behave a bit like the writev() system call.
-  // No return value, function is assumed to always write fully.
-  using WriteFun =
-    std::function<void(std::initializer_list<absl::string_view>)>;
+  using WriteFun = std::function<void(absl::string_view response)>;
 
   // Receive data from the "source", a message stream splitter that calls
   // our DispatchMessage(). If "source" is not given DispatchMessage() can
   // be called manually.
   // Responses are written using the "out" write function.
-  JsonRpcServer(const WriteFun &out, MessageStreamSplitter *source = nullptr)
-    : write_fun_(out) {
-    if (source) {
-      source->SetMessageProcessor([this](absl::string_view /*header*/,
-                                         absl::string_view body) {
-        return DispatchMessage(body);
-      });
-    }
+  JsonRpcServer(const WriteFun &out) : write_fun_(out) {
   }
 
-  void AddHandler(const std::string& method_name, const RPCHandler &fun) {
+  void AddRequestHandler(const std::string& method_name,
+                         const RPCHandler &fun) {
     handlers_.insert({method_name, fun});
+  }
+
+  void AddNotificationHandler(const std::string& method_name,
+                              const RPCNotification &fun) {
+    notifications_.insert({method_name, fun});
   }
 
   const StatsMap& GetStatCounters() const { return statistic_counters_; }
@@ -54,53 +49,75 @@ public:
   //
   // This function is typically called by the MessageStreamSplitter provided
   // at construction time, but can also be called manually (e.g. for testing).
-  absl::Status DispatchMessage(absl::string_view data) {
-    RequestMessage request;
+  void DispatchMessage(absl::string_view data) {
+    nlohmann::json request;
     try {
       request = nlohmann::json::parse(data);
     }
     catch (const std::exception &e) {
       statistic_counters_[e.what()]++;
-      SendReply(MethodNotFound(request, e.what()));
+      SendReply(CreateError(request, kParseError, e.what()));
     }
 
     // Direct dispatch, later maybe send to thread-pool
-    const auto& found = handlers_.find(request.method);
-    if (found != handlers_.end()) {
-      SendReply(found->second(request));
-    } else {
-      SendReply(MethodNotFound(request, absl::StrCat(request.method,
-                                                     ": not implemented")));
+    const bool is_notification = (request.find("id") == request.end());
+    if (request.find("method") == request.end()) {
+      SendReply(CreateError(request, kMethodNotFound,
+                            "Method required in request"));
+      return;
     }
-    statistic_counters_[request.method]++;
-    return absl::OkStatus();
+    bool handled = false;
+    const std::string &method = request["method"];
+    if (is_notification) {
+      const auto& found = notifications_.find(method);
+      if (found != notifications_.end()) {
+        found->second(request["params"]);
+        handled = true;
+      }
+    } else {
+      const auto& found = handlers_.find(method);
+      if (found != handlers_.end()) {
+        SendReply(found->second(request["params"]));  // wrap result.
+        handled = true;
+      } else {
+        SendReply(CreateError(request, kMethodNotFound,
+                              "method '" + method + "' not found."));
+      }
+    }
+    statistic_counters_[method + (handled ? "" : " (unhandled)")]++;
   }
 
 private:
-  static constexpr absl::string_view kEndHeaderMarker = "\r\n\r\n";
-  static constexpr absl::string_view kContentLengthHeader = "Content-Length: ";
+  static constexpr int kParseError = -32700;
+  static constexpr int kMethodNotFound= -32601;
 
-  static ResponseMessage MethodNotFound(const RequestMessage &req,
-                                        absl::string_view msg) {
-    ResponseMessage response;
-    response.id = req.id;
-    response.error.code = 42;
-    response.error.message.assign(msg.data(), msg.size());
-    return response;
+  static nlohmann::json CreateError(const nlohmann::json &request,
+                                    int code, absl::string_view message) {
+    nlohmann::json result = {
+      {"jsonrpc", "2.0"},
+    };
+    result["error"] = { { "code", code } };
+    if (!message.empty()) {
+      result["error"]["message"] = message;
+    }
+
+    if (request.find("id") != request.end()) {
+      result["id"] = request["id"];
+    }
+
+    return result;
   }
 
-  void SendReply(const ResponseMessage &response) {
-    nlohmann::json j = response;
+  void SendReply(const nlohmann::json &response) {
     std::stringstream out_bytes;
-    out_bytes << j << "\n";
-    const std::string& body = out_bytes.str();
-    const std::string size_str = std::to_string(body.size());
-    write_fun_({ kContentLengthHeader, size_str, kEndHeaderMarker, body });
+    out_bytes << response << "\n";
+    write_fun_(out_bytes.str());
   }
 
   const WriteFun write_fun_;
 
   std::unordered_map<std::string, RPCHandler> handlers_;
+  std::unordered_map<std::string, RPCNotification> notifications_;
   StatsMap statistic_counters_;
 };
 
